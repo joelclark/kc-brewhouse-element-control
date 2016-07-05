3411036d6ecec81d9c712f0408c3e89b3db57b6f
Joel S. Clark <joel@angrybits.com>
2016-07-05 18:38:40 -0400

    add neo prefix to all neopixel functions, add comments

--- element-control/element-control.ino
+++ element-control/element-control.ino
@@ -34,16 +34,15 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 #define PIN_ENCODER_A         3
 #define PIN_ENCODER_B         5
 #define TRINKET_PINx          PIND
-#define PIN_ENCODER_SWITCH    4
+#define PIN_ENCODER_SWITCH    4     // active high, use 4.7K pull-down
+
+// ---------------------------------------------------------------------------------- RGB led management
 
 #define NEO_PIN               6
-#define NEO_NUMPIXELS         8
-#define NEO_PIXEL_BRIGHTNESS  10
+#define NEO_NUMPIXELS         8     // indicator, 5 x element, 2 x pump
+#define NEO_PIXEL_BRIGHTNESS  15    // 0-255
 #define NEO_MAIN_LED_OFFSET   0
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
@@ -51,11 +50,12 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_NUMPIXELS, NEO_PIN, NEO_GRB + NEO_KHZ400);
 
-uint32_t neo_color_off;
-uint32_t neo_color_red;
-uint32_t neo_color_yellow;
-uint32_t neo_color_green;
-uint32_t neo_color_blue;
+// pre-calculate colors
+static uint32_t neo_color_off     = pixels.Color(0, 0, 0);
+static uint32_t neo_color_red     = pixels.Color(NEO_PIXEL_BRIGHTNESS, 0, 0);
+static uint32_t neo_color_yellow  = pixels.Color(NEO_PIXEL_BRIGHTNESS, NEO_PIXEL_BRIGHTNESS, 0);
+static uint32_t neo_color_green   = pixels.Color(0, NEO_PIXEL_BRIGHTNESS, 0);
+static uint32_t neo_color_blue    = pixels.Color(0, 0, NEO_PIXEL_BRIGHTNESS);
 
 // ---------------------------------------------------------------------------------- declarations
 
@@ -67,27 +67,36 @@ uint32_t neo_color_blue;
  * the system.  This pins will go to a relay board and open 
  * and close the low voltage line running from the PID to the SSR.
  */
-#define OFF_PIN -1
-#define BOIL1_PIN 8
-#define BOIL2_PIN 9
-#define HLT1_PIN 10
-#define HLT2_PIN 11
-#define RIMS_PIN 12
+#define OFF_PIN                       -1
+#define BOIL1_PIN                     8
+#define BOIL2_PIN                     9
+#define HLT1_PIN                      10
+#define HLT2_PIN                      11
+#define RIMS_PIN                      12
+#define MAX_ELEMENT_PIN               RIMS_PIN
+#define MAX_ELEMENT_LABEL_SIZE        6
 
-#define ELEMENT_COUNT 5
-#define OFF_LED_OFFSET -1
-#define BOIL1_LED_OFFSET 1
-#define BOIL2_LED_OFFSET 2
-#define HLT1_LED_OFFSET 3
-#define HLT2_LED_OFFSET 4
-#define RIMS_LED_OFFSET 5
+#define ELEMENT_COUNT                 5
+#define OFF_LED_OFFSET                -1
+#define BOIL1_LED_OFFSET              1
+#define BOIL2_LED_OFFSET              2
+#define HLT1_LED_OFFSET               3
+#define HLT2_LED_OFFSET               4
+#define RIMS_LED_OFFSET               5
 
-#define ELEMENT_OFF LOW
-#define ELEMENT_ON HIGH
+#define ELEMENT_OFF                   LOW
+#define ELEMENT_ON                    HIGH
 
-static char element_configuration_labels[7][20];
+#define ELEMENT_CONFIGURATION_COUNT   7
 
-static int element_configuration_pins[7][2] = {
+#define UI_MODE_RUNNING               0
+#define UI_MODE_CHOOSING              1
+#define UI_MODE_TIMEOUT               8000 //ms
+#define UI_MODE_TOGGLE_TIME           250  //ms
+
+static char element_configuration_labels[ELEMENT_CONFIGURATION_COUNT][20];
+
+static int element_configuration_pins[ELEMENT_CONFIGURATION_COUNT][2] = {
   {   OFF_PIN,   OFF_PIN },
   {  RIMS_PIN,   OFF_PIN },
   { BOIL1_PIN, BOIL2_PIN },
@@ -97,7 +106,7 @@ static int element_configuration_pins[7][2] = {
   {  HLT1_PIN,  HLT2_PIN }
 };
 
-static int element_configuration_led_offsets[7][2] = {
+static int element_configuration_led_offsets[ELEMENT_CONFIGURATION_COUNT][2] = {
   {   OFF_LED_OFFSET,   OFF_LED_OFFSET },
   {  RIMS_LED_OFFSET,   OFF_LED_OFFSET },
   { BOIL1_LED_OFFSET, BOIL2_LED_OFFSET },
@@ -110,16 +119,11 @@ static int element_configuration_led_offsets[7][2] = {
 static int element_pins[ELEMENT_COUNT] = { BOIL1_PIN, BOIL2_PIN, HLT1_PIN, HLT2_PIN, RIMS_PIN };
 static int element_leds[ELEMENT_COUNT] = { BOIL1_LED_OFFSET, BOIL2_LED_OFFSET, HLT1_LED_OFFSET, HLT2_LED_OFFSET, RIMS_LED_OFFSET };
 
-static char * element_names_by_pin[14];
+static char * element_names_by_pin[MAX_ELEMENT_PIN + 1];
 
 /*
  * These variables will house the state for managing the configuration 
  */
- 
-#define UI_MODE_RUNNING 0
-#define UI_MODE_CHOOSING 1
-#define UI_MODE_TIMEOUT 8000
-#define UI_MODE_TOGGLE_TIME 250
 
 static int proposed_element_configuration = 0;
 static int actual_element_configuration = proposed_element_configuration;
@@ -140,13 +144,6 @@ void setup()
     digitalWrite(element_pins[i], ELEMENT_OFF);
   }
   
-  // pre-calculate colors
-  neo_color_off = pixels.Color(0, 0, 0);
-  neo_color_red = pixels.Color(NEO_PIXEL_BRIGHTNESS, 0, 0);
-  neo_color_yellow = pixels.Color(NEO_PIXEL_BRIGHTNESS, NEO_PIXEL_BRIGHTNESS, 0);
-  neo_color_green = pixels.Color(0, NEO_PIXEL_BRIGHTNESS, 0);
-  neo_color_blue = pixels.Color(0, 0, NEO_PIXEL_BRIGHTNESS);
-
   // allocate strings for the element mode labels
   strcpy((char *) &element_configuration_labels[0], "  OFF / OFF  ");
   strcpy((char *) &element_configuration_labels[1], " RIMS / OFF  ");
@@ -156,11 +153,11 @@ void setup()
   strcpy((char *) &element_configuration_labels[5], " RIMS / HLT1 ");
   strcpy((char *) &element_configuration_labels[6], " HLT1 / HLT2 ");
 
-  element_names_by_pin[BOIL1_PIN] = (char *) malloc(6 * sizeof(char));
-  element_names_by_pin[BOIL2_PIN] = (char *) malloc(6 * sizeof(char));
-  element_names_by_pin[HLT1_PIN]  = (char *) malloc(6 * sizeof(char));
-  element_names_by_pin[HLT2_PIN]  = (char *) malloc(6 * sizeof(char));
-  element_names_by_pin[RIMS_PIN]  = (char *) malloc(6 * sizeof(char));
+  element_names_by_pin[BOIL1_PIN] = (char *) malloc(MAX_ELEMENT_LABEL_SIZE * sizeof(char));
+  element_names_by_pin[BOIL2_PIN] = (char *) malloc(MAX_ELEMENT_LABEL_SIZE * sizeof(char));
+  element_names_by_pin[HLT1_PIN]  = (char *) malloc(MAX_ELEMENT_LABEL_SIZE * sizeof(char));
+  element_names_by_pin[HLT2_PIN]  = (char *) malloc(MAX_ELEMENT_LABEL_SIZE * sizeof(char));
+  element_names_by_pin[RIMS_PIN]  = (char *) malloc(MAX_ELEMENT_LABEL_SIZE * sizeof(char));
   
   strcpy(element_names_by_pin[BOIL1_PIN], "BOIL1");
   strcpy(element_names_by_pin[BOIL2_PIN], "BOIL2");
@@ -195,6 +192,13 @@ void setup()
 
 void loop()
 { 
+  /*
+   * If in choosing mode, we need to check for timeout (the user must press the confirm button)
+   * and if reached, restore the UI to running mode with no changes.  If timeout not yet reached,
+   * the flashing action of the LEDs needs to be maintained.
+   * 
+   * If in running mode, no need to do anything special.
+   */
   if (ui_mode == UI_MODE_CHOOSING) {
     if (millis() - ui_last_choosing_mode_started_at > UI_MODE_TIMEOUT) {
       proposed_element_configuration = actual_element_configuration;
@@ -205,6 +209,15 @@ void loop()
       }
     }
   }
+
+  /*
+   * Most of the code here came from the example at:
+   *    https://learn.adafruit.com/pro-trinket-rotary-encoder/example-rotary-encoder-volume-control
+   * 
+   * The notable exceptions are that: 
+   *    -- enc_action is consulted to see if the rotary encoder was turned
+   *    -- the encoder push switch is consulted to see if a new setting was confirmed
+   */
   
   int8_t enc_action = 0; // 1 or -1 if moved, sign is direction
  
@@ -272,12 +285,14 @@ void loop()
   enc_prev_pos = enc_cur_pos;
  
   if (enc_action > 0) {
+    // turned to the right
     if (proposed_element_configuration < 6) {
       proposed_element_configuration++;
       begin_choosing();
     }
   }
   else if (enc_action < 0) {
+    // turned to the left
     if (proposed_element_configuration > 0) {
       proposed_element_configuration--;  
       begin_choosing();
@@ -290,6 +305,12 @@ void loop()
     {
       delay(5); // debounce delay
       if (ui_mode == UI_MODE_CHOOSING) {
+
+        /*
+         * The user has confirmed an entry by pressing the encoder's knob.  If the confirmed value is different
+         * then change the value and indicate the change in the UI.
+         */
+        
         bool is_changing = actual_element_configuration != proposed_element_configuration;
         actual_element_configuration = proposed_element_configuration;
         begin_running(is_changing);
@@ -312,16 +333,16 @@ void toggle_choosing_indicator() {
   ui_last_choosing_indicator_toggled_at = millis();
   ui_mode_indicator = !ui_mode_indicator;
   if (ui_mode_indicator) {
-    main_led_blue();
+    neo_main_led_blue();
     set_indicator("?");
-    led_elements_proposed();
+    neo_elements_proposed();
   } else {
     set_indicator(" ");
-    led_elements_off();
-    main_led_off();
+    neo_elements_off();
+    neo_main_led_off();
   }
 }
-  
+
 void sync_proposed_configuration_lcd() {
   if (last_requested_proposed_configuration != proposed_element_configuration) {
     last_requested_proposed_configuration = proposed_element_configuration;
@@ -336,6 +357,7 @@ void sync_actual_configration_to_relay_board() {
   for (int element_offset = 0; element_offset < 2; element_offset++) {
     fire_relay(element_offset);
   }
+  neo_elements_running();
 }
 
 void fire_relay(int element_offset) {
@@ -359,24 +381,27 @@ void all_off() {
   for (int i = 0; i < ELEMENT_COUNT; i++) {
     digitalWrite(element_pins[i], ELEMENT_OFF);
   }
-  led_elements_off();
+  neo_elements_off();
 }
 
 void begin_running(bool is_changing) {
+  
   ui_mode = UI_MODE_RUNNING;
   last_requested_proposed_configuration = -1;
+  
   sync_proposed_configuration_lcd();
   set_indicator(" ");
   sync_actual_configration_to_relay_board();
-  led_elements_running();
+  
   if (is_changing) {
     for (int i = 0; i < 5; i++) {
       main_led_running();
       delay(50);
-      main_led_off();
+      neo_main_led_off();
       delay(50);
     }
   }
+  
   main_led_running();
 }
 
@@ -411,59 +436,63 @@ void neo_show() {
 }
 
 void neo_led_apply(int offset, uint32_t color) {
-  if (offset >= 0) {
-    pixels.setPixelColor(offset, color);
-    neo_show();
-  }
+  neo_led_apply_deferred(offset, color);
+  neo_show();
 }
 
 void main_led_running() {
   if (actual_element_configuration == 0) {
-    main_led_green();
+    neo_main_led_green();
   } else {
-    main_led_red();
+    neo_main_led_red();
   }
 }
 
-void led_elements_off() {
+void neo_elements_off() {
   neo_led_apply_all_elements_deferred(neo_color_off);
   neo_show();
 }
 
-void led_elements_running() {
-  neo_led_apply_all_elements_deferred(neo_color_green);
-  neo_led_apply_deferred(element_configuration_led_offsets[actual_element_configuration][0], neo_color_red);
-  neo_led_apply_deferred(element_configuration_led_offsets[actual_element_configuration][1], neo_color_red);
+void neo_elements_running() {
+  neo_elements_apply_configuration(actual_element_configuration, neo_color_green, neo_color_red, neo_color_green);
+}
+
+void neo_elements_proposed() {
+  neo_elements_apply_configuration(proposed_element_configuration, neo_color_off, neo_color_red, neo_color_green);
+}
+
+void neo_elements_apply_configuration(int configuration, uint32_t off_color, uint32_t on_color, uint32_t config_zero_color) {
+  
+  neo_led_apply_all_elements_deferred(configuration == 0 ? config_zero_color : off_color);
+  
+  if (configuration > 0) {
+    neo_led_apply_deferred(element_configuration_led_offsets[configuration][0], on_color);
+    neo_led_apply_deferred(element_configuration_led_offsets[configuration][1], on_color);
+  }
+  
   neo_show();
 }
 
-void led_elements_proposed() {
-  neo_led_apply_all_elements_deferred((proposed_element_configuration == 0 ? neo_color_green : neo_color_off));
-  neo_led_apply_deferred(element_configuration_led_offsets[proposed_element_configuration][0], neo_color_yellow);
-  neo_led_apply_deferred(element_configuration_led_offsets[proposed_element_configuration][1], neo_color_yellow);
-  neo_show();
-}
-
-void main_led_apply(uint32_t color) {
+void neo_main_led_apply(uint32_t color) {
   neo_led_apply(NEO_MAIN_LED_OFFSET, color);
 }
 
-void main_led_blue() {
-  main_led_apply(neo_color_blue);
+void neo_main_led_blue() {
+  neo_main_led_apply(neo_color_blue);
 }
 
-void main_led_red() {
-  main_led_apply(neo_color_red);
+void neo_main_led_red() {
+  neo_main_led_apply(neo_color_red);
 }
 
-void main_led_green() {
-  main_led_apply(neo_color_green);
+void neo_main_led_green() {
+  neo_main_led_apply(neo_color_green);
 }
 
-void main_led_yellow() {
-  main_led_apply(neo_color_yellow);
+void neo_main_led_yellow() {
+  neo_main_led_apply(neo_color_yellow);
 }
 
-void main_led_off() {
-  main_led_apply(neo_color_off);
+void neo_main_led_off() {
+  neo_main_led_apply(neo_color_off);
 }
